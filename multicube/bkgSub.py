67042f36bd1f6f88e52f512e76dc9027387a9a01
Donal <donal.b.osullivan@gmail.com>
2018-03-14 14:49:25 -0700

    Updated bkgSub to use param['REG_FILE'] instead of command line arg.

--- multicube/bkgSub.py
+++ multicube/bkgSub.py
@@ -1,6 +1,3 @@
-from astropy.io import fits as fitsIO
-from scipy.ndimage.measurements import center_of_mass
-
 import libs
 import numpy as np
 import pyregion
@@ -12,7 +9,6 @@ import matplotlib.pyplot as plt
 #Get user input parameters               
 parampath = sys.argv[1]
 cubetype = sys.argv[2]
-regpath = sys.argv[3]
 
 #Load pipeline parameters
 params = libs.params.loadparams(parampath)
@@ -24,7 +20,9 @@ files = libs.io.findfiles(params,cubetype)
 fits = [libs.fits3D.open(f) for f in files] 
 
 #Open regionfile
-regfile = pyregion.open(regpath)
+regpath = params["REG_FILE"]
+if regpath=='None': print "WARNING: No region file specified in %s. Sources will not be masked."
+else: regfile = pyregion.open(regpath)
 
 #Subtract continuum sources
 for i,f in enumerate(fits):
@@ -36,11 +34,17 @@ for i,f in enumerate(fits):
     fits[i][0].data = np.nan_to_num(f[0].data) 
 
     #Get for region file mask for this fits
-    regmask = libs.cubes.get_mask(f,regfile)
+    if regpath!="None": 
+    
+        #Get 2D Mask based on region file
+        regmask = libs.cubes.get_mask(f,regfile)
+        
+        #Apply median mask to sources    
+        cube_masked = libs.cubes.apply_mask(f[0].data.copy(),regmask,mode='xmedian',inst=params["INST"][i])
 
-    #Apply median mask to sources    
-    cube_masked = libs.cubes.apply_mask(f[0].data.copy(),regmask,mode='xmedian',inst=params["INST"][i])
-
+    #Just use unmasked cube if no region file provided
+    else: cube_masked = f[0].data
+    
     #Run cube-wide polyfit to subtract scattered light    
     wcrop = tuple(int(w) for w in params["WCROP"][i].split(':'))
     polyfit = libs.continuum.polyModel(cube_masked,w0=wcrop[0],w1=wcrop[1])
