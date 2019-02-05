0023250c97a5ab218581cb6357e3171e1aa5f6a5
Donal <donal.b.osullivan@gmail.com>
2019-02-04 22:30:24 -0800

    Added argparse to coadd.py

--- coadd.py
+++ coadd.py
@@ -11,6 +11,7 @@
 #
 
 from astropy.io import fits as fitsIO
+import argparse
 import numpy as np
 import sys
 import time
@@ -19,49 +20,73 @@ import libs
 #Timer start
 tStart = time.time()
 
-#Get user input parameters               
-parampath = sys.argv[1]
-cubetype = sys.argv[2]
+# Use python's argparse to handle command-line input
+parser = argparse.ArgumentParser(description='Coadd data cubes.')
 
-#Take any additional input params, if provided
-settings = {"pixelThreshold":0.9,"expThreshold":0.75,"propVar":True}
-if len(sys.argv)>3:
-    for item in sys.argv[3:]:      
-        key,val = item.split('=')
-        if settings.has_key(key):
-            if "Threshold" in key: val=float(val)
-            settings[key]=val
-        else:
-            print "Input argument not recognized: %s" % key
-            sys.exit()
-            
-#Add file extension of omitted
-if not ".fits" in cubetype: cubetype += ".fits"
+mainGroup = parser.add_argument_group(title="Main",description="Basic input")
+mainGroup.add_argument('paramFile', 
+                    type=str, 
+                    metavar='Parameter File',             
+                    help='CWITools Parameter file (used to load cube list etc.)'
+)
+mainGroup.add_argument('cubeType', 
+                    type=str, 
+                    metavar='Cube Type',             
+                    help='The type of cube (i.e. file extension such as \'icubed.fits\) to coadd'
+)
 
-#Check if any parameter values are missing (set to set-up mode if so)
-params = libs.params.loadparams(parampath)
+methodGroup = parser.add_argument_group(title="Methods",description="Parameters related to coadd methods.")
+methodGroup.add_argument('-pxThresh',
+                    type=float,
+                    metavar='Pixel Threshold',
+                    help='Fraction of a coadd-frame pixel that must be covered by an input frame to be included (0-1)',
+                    default=0.9
+)
+methodGroup.add_argument('-expThresh',
+                    type=float,
+                    metavar='Exposure Threshold',
+                    help='Crop coadded cube to include only spaxels with this fraction of the maximum overlap in exposure time.',
+                    default=0.75
+)
+
+fileIOGroup = parser.add_argument_group(title="File I/O",description="File input/output options.")
+fileIOGroup.add_argument('-propVar',
+                    type=bool,
+                    metavar='Propagate Error',
+                    help='Propagate error through coadd process (i.e. coadding variance cubes.)',
+                    default=False
+)
+args = parser.parse_args()
+             
+#Try to load the param file
+try: params = libs.params.loadparams(args.paramFile)
+except: print("Error: could not open '%s'\nExiting."%args.cube);sys.exit()
 
 #Check if parameters are complete
 libs.params.verify(params)
 
+  
+#Add file extension of omitted
+if not ".fits" in args.cubeType: args.cubeType += ".fits"
+
 #Get filenames     
-files = libs.io.findfiles(params,cubetype)
+files = libs.io.findfiles(params,args.cubeType)
 
 #Stack cubes and trim
-stackedFITS,varFITS = libs.cubes.coadd(files,params,settings)  
+stackedFITS,varFITS = libs.cubes.coadd(files,params,expThresh=args.expThresh,pxThresh=args.pxThresh,propVar=args.propVar)  
 
 #Add redshift info to header
 stackedFITS[0].header["Z"] = params["Z"]
 stackedFITS[0].header["ZLA"] = params["ZLA"]
 
 #Save stacked cube
-stackedpath = '%s%s_%s' % (params["PRODUCT_DIR"],params["NAME"],cubetype)
+stackedpath = '%s%s_%s' % (params["PRODUCT_DIR"],params["NAME"],args.cubeType)
 stackedFITS[0].writeto(stackedpath,overwrite=True)
 print "\nSaved %s" % stackedpath
 
 #Save variance cube if one was returned
 if varFITS!=None:
-    varpath = '%s%s_%s' % (params["PRODUCT_DIR"],params["NAME"],cubetype.replace("icube","vcube"))
+    varpath = '%s%s_%s' % (params["PRODUCT_DIR"],params["NAME"],args.cubeType.replace("icube","vcube"))
     varFITS[0].writeto(varpath,overwrite=True)
     print "Saved %s" % varpath    
     
