a840046c4cebc8cd1e07c5915b38553d1eedc06c
dbosul <dosulliv@caltech.edu>
2019-08-24 20:09:00 -0700

    Added argparse and removed raw_input at the end.

--- fillParams.py
+++ fillParams.py
@@ -1,26 +1,35 @@
-#!/usr/bin/env python
-
 from astropy.io import fits as fitsIO
+
+import argparse
 import numpy as np
 import sys
 import time
+
 import libs
 
-#Timer start
-tStart = time.time()
+parser = argparse.ArgumentParser(description='Auto-fill certain values in parameter file using header data.')
+parser.add_argument('paramPath',
+                    type=str,
+                    help='Path to CWITools parameter file to be filled.'
+)
+parser.add_argument('cubeType',
+                    type=str,
+                    help='Type of PCWI/KCWI cube to load for filling parameters. ("e.g. icubes.fits, ocubes.fits") Should end in .fits'
+)
+args = parser.parse_args()
 
 #Get user input parameters
-parampath = sys.argv[1]
-cubetype = sys.argv[2]
+paramPath = args.paramPath
+cubeType = args.cubeType
 
 #Add file extension of omitted
-if not ".fits" in cubetype: cubetype += ".fits"
+if not ".fits" in cubeType: cubeType += ".fits"
 
 #Check if any parameter values are missing (set to set-up mode if so)
-params = libs.params.loadparams(parampath)
+params = libs.params.loadparams(paramPath)
 
 #Get filenames
-files = libs.io.findfiles(params,cubetype)
+files = libs.io.findfiles(params,cubeType)
 
 #Open custom FITS-3D objects
 fits = [fitsIO.open(f) for f in files] 
@@ -31,8 +40,5 @@ libs.params.verify(params)
 #2 - re-initialize param values from FITS headers
 params = libs.params.parseHeaders(params,fits)
 
-
 #3 - overwrite param file
-params = libs.params.writeparams(params,parampath)
-
-raw_input("")
+params = libs.params.writeparams(params,paramPath)
