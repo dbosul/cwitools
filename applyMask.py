54d50e470e30dd910e0d70688d50b64c32966929
Donal <donal.b.osullivan@gmail.com>
2019-08-26 15:31:14 -0700

    Cleaning out v0.1 branch

--- applyMask.py
+++
@@ -1,8 +0,0 @@
-from astropy.io import fits
-import sys
-
-mskFITS = fits.open(sys.argv[1])
-inpFITS = fits.open(sys.argv[2])
-
-inpFITS[0].data *= (mskFITS[0].data==0)
-inpFITS.writeto(sys.argv[2].replace('.fits','.M.fits'),overwrite=True)
