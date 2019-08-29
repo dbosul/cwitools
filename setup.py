73b5928f2a61b211dfd6b2c2b0dbada3a923fc67
dbosul <donal.b.osullivan@gmail.com>
2019-08-29 15:23:07 -0700

    Added entry points to setup.py

--- setup.py
+++ setup.py
@@ -8,13 +8,20 @@ setuptools.setup(
         version='0.1',
         author="Donal O'Sullivan",
         author_email="dosulliv@caltech.edu",
-        description="Analysis and Reduction for CWI Data",
+        description="Analysis and Reduction Tools for PCWI/KCWI Data",
         long_description=long_description,
         url="https://github.com/dbosul/cwitools",
         packages=setuptools.find_packages(),
-        # entry_points = {
-        #     'console_scripts': [
-        #         'cwi_test01 = cwitools.analysis.test:main'
-        #     ]
-        # }
+        entry_points = {
+             'console_scripts': [
+                 'cwi_applymask = cwitools.scripts.cwi_applymask:main',
+                 'cwi_bgsub = cwitools.scripts.cwi_bgsub:main',
+                 'cwi_coadd = cwitools.scripts.cwi_coadd:main',
+                 'cwi_crop = cwitools.scripts.cwi_crop:main',
+                 'cwi_fixwcs = cwitools.scripts.cwi_fixwcs:main',
+                 'cwi_getvar = cwitools.scripts.cwi_getvar:main',
+                 'cwi_psfsub = cwitools.scripts.cwi_psfsub:main',
+                 'cwi_rebin = cwitools.scripts.cwi_rebin:main'
+             ]
+        }
 )
