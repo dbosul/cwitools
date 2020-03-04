f17a39e136b0ae034b1752f440a81c1ae77e02dd
Donal <donal.b.osullivan@gmail.com>
2020-03-03 19:04:26 -0800

    Updated setup

--- setup.py
+++ setup.py
@@ -5,22 +5,25 @@ with open("README.md","r") as fh:
 
 setuptools.setup(
         name='cwitools',
-        version='0.1dev11',
+        version='0.5',
         author="Donal O'Sullivan",
         author_email="dosulliv@caltech.edu",
         description="Analysis and Reduction Tools for PCWI/KCWI Data",
         long_description=long_description,
         url="https://github.com/dbosul/cwitools",
-        download_url="https://github.com/dbosul/cwitools/archive/v0.1dev11.tar.gz",
+        download_url="https://github.com/dbosul/cwitools/archive/v0.5.tar.gz",
         packages=setuptools.find_packages(),
         entry_points = {
              'console_scripts': [
                  'cwi_applymask = cwitools.scripts.cwi_applymask:main',
+                 'cwi_applywcs = cwitools.scripts.cwi_applywcs:main',
                  'cwi_bgsub = cwitools.scripts.cwi_bgsub:main',
                  'cwi_coadd = cwitools.scripts.cwi_coadd:main',
                  'cwi_crop = cwitools.scripts.cwi_crop:main',
-                 'cwi_fixwcs = cwitools.scripts.cwi_fixwcs:main',
+                 'cwi_getmask = cwitools.scripts.cwi_getmask:main',
                  'cwi_getvar = cwitools.scripts.cwi_getvar:main',
+                 'cwi_measurewcs = cwitools.scripts.cwi_measurewcs:main',
+                 'cwi_moments = cwitools.scripts.cwi_moments:main',
                  'cwi_psfsub = cwitools.scripts.cwi_psfsub:main',
                  'cwi_rebin = cwitools.scripts.cwi_rebin:main'
              ]
@@ -34,7 +37,5 @@ setuptools.setup(
         'scipy',
         'shapely',
         'tqdm',
-
-
         ]
 )
