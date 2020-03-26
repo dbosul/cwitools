e2fb0a643a3bcbea906b35ba0122e234185d86ce
Donal <donal.b.osullivan@gmail.com>
2020-03-26 13:06:18 -0700

    Updated output() and removed log_command (was redundant)

--- cwitools/utils.py
+++ cwitools/utils.py
@@ -20,35 +20,6 @@ def get_fits(data, header=None):
 def set_cmdlog(path):
     cwitools.command_log = path
 
-def log_command(sys_argv, logfile=None):
-    """Append a terminal command reflected by sys.argv to the log file
-
-    Args:
-        sys_argv: The sys.argv attribute from the script that was called.
-
-    Returns:
-        int: 1 if successfully written to file, -1 if failed.
-    """
-    cmd_string = " ".join(sys.argv)
-
-    #If no log-file given, warn user and return bad flag
-    if logfile == None:
-        warnings.warn("Command will not be saved. No log file given.")
-        return -1
-
-    #Let user know if new file is being made (can help avoid typos)
-    if not(os.path.isfile(logfile)):
-        warnings.warn("%s does not exist. File will be created." % logfile)
-
-    try:
-        cmd_log = open(logfile, 'a')
-        cmd_log.write(cmd_string + '\n')
-        cmd_log.close()
-
-    except:
-        raise ValueError("Error opening/writing to log file: %s" % logfile)
-
-    return 1
 
 def find_files(id_list, datadir, cubetype, depth=3):
     """Finds the input files given a CWITools parameter file and cube type.
@@ -151,3 +122,27 @@ def parse_cubelist(filepath):
         raise ValuError("Could not parse SEARCH_DEPTH to int (%s)" % clist["SEARCH_DEPTH"])
     #Return the dictionary
     return clist
+
+def output(str, log=None, silent=True):
+
+    uselog = True
+
+    #First priority, take given log
+    if log != None:
+        logfilename = log
+
+    #Second priority, take global log file
+    elif cwitools.script_log != None:
+        logfilename = cwitools.script_log
+
+    #If neither log set, ignore
+    else:
+        uselog = False
+
+    if not silent:
+        print(str, end='')
+
+    if uselog:
+        logfile = open(logfilename, 'a')
+        logfile.write(str)
+        logfile.close()
