19c4174dce03aa76d021afe9e11fa56d1c0bc694
Donal <donal.b.osullivan@gmail.com>
2020-03-26 13:05:27 -0700

    Updated metavariables in help menu.

--- cwitools/scripts/cwi_coadd.py
+++ cwitools/scripts/cwi_coadd.py
@@ -1,45 +1,45 @@
 """Stack input cubes into a master frame using a CWITools parameter file."""
 from astropy.io import fits
-from cwitools import parameters, reduction, utils
+from cwitools import reduction, utils
 
 import argparse
 import os
+import sys
 import time
 
 def main():
 
     #Timer start
-    tStart = time.time()
+    tstart = time.time()
 
     # Use python's argparse to handle command-line input
     parser = argparse.ArgumentParser(description='Coadd data cubes.')
 
-    modeGroup = parser.add_mutually_exclusive_group(required=True)
-    modeGroup.add_argument('-cubelist',
+    parser.add_argument('list',
                         type=str,
-                        help='A comma-separated list of cubes.'
-    )
-    modeGroup.add_argument('-param',
-                        type=str,
-                        help='A CWITools Parameter file.'
+                        help='Comma-separated list of FITS files or CWITools .list file.'
     )
 
     methodGroup = parser.add_argument_group(title="Methods",description="Parameters related to coadd methods.")
-    methodGroup.add_argument('-cubetype',
+    methodGroup.add_argument('-ctype',
+                        metavar="<cube_type>",
                         type=str,
                         help='The type of cube (i.e. file extension such as \'icubed.fits\') to coadd'
     )
     methodGroup.add_argument('-pxthresh',
+                        metavar="0-1",
                         type=float,
                         help='Fraction of a coadd-frame pixel that must be covered by an input frame to be included (0-1)',
                         default=0.5
     )
     methodGroup.add_argument('-expthresh',
+                        metavar="0-1",
                         type=float,
                         help='Crop cube to include only spaxels with this fraction of the maximum overlap (0-1)',
                         default=0.75
     )
     methodGroup.add_argument('-pa',
+                        metavar="<dd.dd>",
                         type=float,
                         help='Position Angle of output frame.',
                         default=0
@@ -50,74 +50,63 @@ def main():
                         action='store_true'
     )
     fileIOGroup.add_argument('-out',
+                        metavar="<file_out>",
                         type=str,
                         help='Output file name.',
                         default=None
     )
     fileIOGroup.add_argument('-v',help="Show progress and file names.",action='store_true')
     fileIOGroup.add_argument('-log',
+                        metavar="<log_file>",
                         type=str,
                         help="Log file to save this command in",
-                        def=None
+                        default=None
     )
     args = parser.parse_args()
 
-    utils.log_command(sys.argv, logfile=args.log)
+    if args.out != None:
+        outfile = args.out
 
-    if args.cubelist==None and args.cubetype==None:
-        raise RuntimeError("Must provide -cubetype if using -param.")
-
-    #Make list out of single cube if working in that mode
-    if args.cubelist!=None and args.param==None and args.cubetype==None:
-        fileList = []
-        cubes = args.cubelist.split(',')
-        for cubePath in cubes:
-            if os.path.isfile(cubePath): fileList.append(cubePath)
+    if "," in args.list:
+        cubes = args.list.split(',')
+        file_list = []
+        for x in cubes:
+            if os.path.isfile(x):
+                file_list.append(x)
             else:
-                raise FileNotFoundError(cubePath)
+                raise FileNotFoundError(x)
 
-        if args.out==None:
-            outFileName = fileList[0].replace('.fits','_coadd.fits')
-        else:
-            outFileName = args.out
+        #Get output name from first file
+        if args.out == None:
+            outfile = file_list[0].replace(".fits", "_coadd.fits")
 
-    elif args.cubelist==None and args.param!=None and args.cubetype!=None:
+    elif os.path.isfile(args.list) and args.ctype != None:
 
-        # Check if any parameter values are missing (set to set-up mode if so)
-        if os.path.isfile(args.param): params = parameters.load_params(args.param)
-        else:
-            raise FileNotFoundError(args.param)
-
-        # Get filenames
-        fileList = parameters.find_files(
-            params["ID_LIST"],
-            params["INPUT_DIRECTORY"],
-            args.cubetype,
-            depth=params["SEARCH_DEPTH"]
+        clist = utils.parse_cubelist(args.list)
+        file_list = utils.find_files(
+            clist["ID_LIST"],
+            clist["INPUT_DIRECTORY"],
+            args.ctype,
+            depth=clist["SEARCH_DEPTH"]
         )
 
-
-        #Make output filename
+        #Get output name from list name
         if args.out==None:
-            outFileName = '%s%s_%s' % (params["OUTPUT_DIRECTORY"],params["TARGET_NAME"],args.cubetype)
-        else:
-            outFileName = args.out
+            name = args.list.split(".")[0]
+            outfile = '%s%s_%s' % (clist["OUTPUT_DIRECTORY"], name, args.ctype)
 
-    #Make sure usage is understood if some odd mix
     else:
-        raise SyntaxError("""\n\nUsage should be one of the following modes:\
-        \n\nUse -cubelist flag to manually specify a list of cubes to coadd. Must be comma-separated.\
-        \nOR\
-        \nUse -params AND -cubetype flag together to load/coadd cubes with a parameter file.
-        """)
+        raise SyntaxError("Syntax should be either a comma-separated list of\
+        files to coadd or a CWITools .list file along with -ctype.")
 
-    fitsList = [fits.open(x) for x in fileList]
 
-    if len(fitsList) == 0:
+    fits_list = [fits.open(x) for x in file_list]
+
+    if len(fits_list) == 0:
         raise RuntimeError("No files matching search found. Check usage and try again.")
 
     #Coadd the fits files
-    stackedFITS = reduction.coadd(fitsList,
+    coadd_fits = reduction.coadd(fits_list,
                       pxthresh=args.pxthresh,
                       expthresh=args.expthresh,
                       pa=args.pa,
@@ -127,12 +116,14 @@ def main():
 
     #Save stacked cube
 
-    stackedFITS.writeto(outFileName,overwrite=True)
+    coadd_fits.writeto(outfile,overwrite=True)
 
     #Timer end
-    tFinish = time.time()
-    print("\nSaved %s" % outFileName)
-    print("Elapsed time: %.2f seconds" % (tFinish-tStart))
+    tfinish = time.time()
+    print("\nSaved %s" % outfile)
+    print("Elapsed time: %.2f seconds" % (tfinish-tstart))
+
+    utils.log_command(sys.argv, logfile=args.log)
 
 
 if __name__=="__main__": main()
