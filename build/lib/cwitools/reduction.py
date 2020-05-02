e8a815d9afd7a90559588f5315c00f360a0d357d
yuguangchen1 <yuguang.chen.1@gmail.com>
2020-05-02 11:04:43 -0700

    Adding cynlindrical projection

--- build/lib/cwitools/reduction.py
+++ build/lib/cwitools/reduction.py
@@ -5,6 +5,8 @@ from astropy.io import fits
 from astropy.wcs import WCS
 from astropy.wcs.utils import proj_plane_pixel_scales
 import astropy.stats
+import astropy.coordinates
+from PyAstronomy import pyasl
 from scipy.interpolate import interp1d
 from scipy.ndimage.filters import convolve
 from scipy.ndimage.measurements import center_of_mass
@@ -1507,3 +1509,150 @@ plot=False):
 
     #Create FITS for variance data if we are propagating that
     return coaddFITS
+
+
+
+
+def air2vac(fits_in,mask=False):
+    """Covert wavelengths in a cube from standard air to vacuum.
+    
+    Args:
+        fits_in (astropy HDU / HDUList): Input HDU/HDUList with 3D data. 
+        mask (bool): Set if the cube is a mask cube.
+
+    Returns:
+        HDU / HDUList*: Trimmed FITS object with updated header.
+        *Return type matches type of fits_in argument.
+        
+    """
+    
+    hdu=utils.extractHDU(fits_in)
+    hdu=hdu.copy()
+    cube=np.nan_to_num(hdu.data,nan=0,posinf=0,neginf=0)
+    hdr=hdu.header
+    
+    if hdr['CTYPE3']=='WAVE':
+        utils.output("\tFITS already in vacuum wavelength.\n")
+        return fits_in
+    
+    wave_air=coordinates.get_wav_axis(hdr)
+    wave_vac=pyasl.airtovac2(wave_air)
+    
+    # resample to uniform grid
+    cube_new=np.zeros_like(cube)
+    for i in range(cube.shape[2]):
+        for j in range(cube.shape[1]):
+            spec0=cube[:,j,i]
+            if mask==False:
+                f_cubic=interp1d(wave_vac,spec0,kind='cubic',fill_value='extrapolate')
+                spec_new=f_cubic(wave_air)
+            else:
+                f_pre=interp1d(wave_vac,spec0,kind='previous',bounds_error=False,fill_value=128)
+                spec_pre=f_pre(wave_air)
+                f_nex=interp1d(wave_vac,spec0,kind='next',bounds_error=False,fill_value=128)
+                spec_nex=f_nex(wave_air)
+                
+                spec_new=np.zeros_like(spec0)
+                for k in range(spec0.shape[0]):
+                    spec_new[k]=max(spec_pre[k],spec_nex[k])
+            cube_new[:,j,i]=spec_new
+    
+    hdr['CTYPE3']='WAVE'
+
+    hdu_new=utils.matchHDUType(fits_in, cube_new, hdr)
+
+    return hdu_new
+    
+
+    
+def heliocentric(fits_in, mask=False, return_vcorr=False, resample=True, vcorr=None, barycentric=False):
+    """Apply heliocentric correction to the cubes.
+    
+    Args:
+        fits_in (astropy HDU / HDUList): Input HDU/HDUList with 
+            3D data. 
+        mask (bool): Set if the cube is a mask cube. This only 
+            works for resampled cubes. 
+        return_vcorr (bool): If set, return the correction velocity 
+            (in km/s) as well.
+        resample (bool): Resample the cube to the original wavelength 
+            grid?
+        vcorr (float): Use a different correction velocity.
+        barycentric (bool): Use barycentric correction instead of helocentric.
+        
+
+    Returns:
+        HDU / HDUList*: Trimmed FITS object with updated header.
+        vcorr (float): Correction velocity in km/s. Only returns if vcorr 
+            is set to True.
+        *Return type matches type of fits_in argument.
+        
+    """
+    
+    hdu=utils.extractHDU(fits_in)
+    hdu=hdu.copy()
+    cube=np.nan_to_num(hdu.data,nan=0,posinf=0,neginf=0)
+    hdr=hdu.header
+    
+    v_old=0.
+    if 'VCORR' in hdr:
+        v_old=hdr['VCORR']
+        utils.output("\tRolling back the existing correction with:\n")
+        utils.output("\t\tVcorr = %.2f km/s.\n" % (v_old))
+    
+    if vcorr is None:
+        targ=astropy.coordinates.SkyCoord(hdr['TARGRA'],hdr['TARGDEC'],unit='deg',obstime=hdr['DATE-BEG'])
+        keck=astropy.coordinates.EarthLocation.of_site('Keck Observatory')
+        if barycentric:
+            vcorr=targ.radial_velocity_correction(kind='barycentric',location=keck)
+        else:
+            vcorr=targ.radial_velocity_correction(kind='heliocentric',location=keck)
+        vcorr=vcorr.to('km/s').value
+        
+    utils.output("\tHelio/Barycentric correction:\n")
+    utils.output("\t\tVcorr = %.2f km/s.\n" % (vcorr))
+            
+    v_tot=vcorr-v_old
+    
+    if resample==False:
+        hdr['CRVAL3']=hdr['CRVAL3']*(1+v_tot/2.99792458e5)
+        hdr['CD3_3']=hdr['CD3_3']*(1+v_tot/2.99792458e5)
+        hdr['VCORR']=vcorr
+        hdu_new=utils.matchHDUType(fits_in, cube, hdr)
+        if not return_vcorr:
+            return hdu_new
+        else:
+            return hdu_new,vcorr
+
+    else:
+    
+        wave_old=coordinates.get_wav_axis(hdr)
+        wave_hel=wave_old*(1+v_tot/2.99792458e8)
+
+        # resample to uniform grid
+        cube_new=np.zeros_like(cube)
+        for i in range(cube.shape[2]):
+            for j in range(cube.shape[1]):
+                spec0=cube[:,j,i]
+                if mask==False:
+                    f_cubic=interp1d(wave_hel,spec0,kind='cubic',fill_value='extrapolate')
+                    spec_new=f_cubic(wave_old)
+                else:
+                    f_pre=interp1d(wave_hel,spec0,kind='previous',bounds_error=False,fill_value=128)
+                    spec_pre=f_pre(wave_old)
+                    f_nex=interp1d(wave_hel,spec0,kind='next',bounds_error=False,fill_value=128)
+                    spec_nex=f_nex(wave_old)
+
+                    spec_new=np.zeros_like(spec0)
+                    for k in range(spec0.shape[0]):
+                        spec_new[k]=max(spec_pre[k],spec_nex[k])
+                cube_new[:,j,i]=spec_new
+
+        hdr['VCORR']=vcorr
+        hdu_new=utils.matchHDUType(fits_in, cube_new, hdr)
+
+        if not return_vcorr:
+            return hdu_new
+        else:
+            return hdu_new,vcorr
+    
