d997965b7816bf6a5cf127d753d28b9b405d38ba
dbosul <dosulliv@caltech.edu>
2020-05-04 13:03:33 -0700

    Added scripts.shellaccess to provide access to scripts from shells

---
+++ cwitools/scripts/shellaccess.py
@@ -0,0 +1,6 @@
+import cwitools.scripts as scripts
+
+def apply_mask(mask=None, data=None, fill=0, ext=".M.fits", log=None,
+    silent=False, label=0):
+    """Wrapper for applying mask to data - Docstring TBC"""
+    scripts.apply_mask.main(mask, data, fill, ext, log, silent, label)
