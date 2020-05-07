1c74fdfbf16a7bdd95196412c2c00c9c0e226aa6
dbosul <dosulliv@caltech.edu>
2020-05-06 19:23:46 -0700

    Added trim_mode, trim_sclip, auto_pad and changed auto settings

--- cwitools/scripts/crop.py
+++ cwitools/scripts/crop.py
@@ -33,20 +33,38 @@ def main():
     parser.add_argument('-wcrop',
                         metavar="<w0:w1>",
                         type=str,
-                        help="Wavelength range, in Angstrom, to crop to (syntax 'w0,w1') (Default:0,-1).",
-                        default='0:-1'
+                        help="Wavelength range, in Angstrom, to crop to (syntax 'w0:w1') or 'auto' for automatic cropping.",
+                        default='auto'
     )
     parser.add_argument('-xcrop',
                         metavar="<x0:x1>",
                         type=str,
-                        help="Subrange of x-axis to crop to (syntax 'x0,x1') (Default:0,-1)",
-                        default='0:-1'
+                        help="Subrange of x-axis to crop to (syntax 'x0:x1') or 'auto' for automatic cropping. Default: auto",
+                        default='auto'
     )
     parser.add_argument('-ycrop',
                         metavar="<y0:y1>",
                         type=str,
-                        help="Subrange of y-axis to crop to (syntax 'y0,y1') (Default:0,-1)",
-                        default='0:-1'
+                        help="Subrange of y-axis to crop to (syntax 'y0:y1') or 'auto' for automatic cropping. Default: auto.",
+                        default='auto'
+    )
+    parser.add_argument('-trim_mode',
+                        metavar="<zero/edge>",
+                        type=str,
+                        help="Trim mode to use when cropping x/y with 'auto'. 'zero' trims empty rows/columns. 'edge' detects and removes edge features.",
+                        default='zero'
+    )
+    parser.add_argument('-trim_sclip',
+                        metavar="<sigma_clip>",
+                        type=float,
+                        help="Sigma-clipping threshold to use on slices if using 'auto' with -trim_mode 'edge'",
+                        default=3
+    )
+    parser.add_argument('-auto_pad',
+                        metavar="<1[,1]>",
+                        type=str,
+                        help="Additional margin (px) on axes 1 and 2 to add to automatically determined crop parameters. Can be an integer or comma-separated integer tuple. Default 0.",
+                        default="0"
     )
     parser.add_argument('-ext',
                         metavar='<file_ext>',
@@ -54,12 +72,8 @@ def main():
                         help='The filename extension to add to cropped cubes. Default: .c.fits',
                         default=".c.fits"
     )
-    parser.add_argument('-auto',
-                        help="Automatically determine ALL crop settings. Overrides other parameters.",
-                        action='store_true'
-    )
     parser.add_argument('-plot',
-                        help="Show profiles of each axis with crop region overlaid.",
+                        help="Show automatically determined plot parameters, if using 'auto' for any.",
                         action='store_true'
     )
     parser.add_argument('-log',
@@ -111,31 +125,64 @@ def main():
         \n\tGive a comma-separated list of cube types (e.g. icubes.fits) and the -list argument
         """)
 
-    #Parse crop parameters
-    try: x0, x1 = (int(x) for x in args.xcrop.split(':'))
-    except:
-        raise ValueError("Could not parse -xcrop, should be colon-separated integer tuple.")
+    #Assign auto parameters or parse user input as needed
 
-    try: y0, y1 = (int(y) for y in args.ycrop.split(':'))
-    except:
-        raise ValueError("Could not parse -ycrop, should be colon-separated integer tuple.")
+    if args.xcrop.lower() != 'auto':
+        try:
+            xcrop = tuple(int(x) for x in args.xcrop.split(':'))
+        except:
+            raise ValueError("Could not parse -xcrop, should be colon-separated integer tuple.")
 
-    try: w0, w1 = (int(w) for w in args.wcrop.split(':'))
+    if args.ycrop.lower() != 'auto':
+        try:
+            ycrop = tuple(int(y) for y in args.ycrop.split(':'))
+        except:
+            raise ValueError("Could not parse -ycrop, should be colon-separated integer tuple.")
+
+    if args.wcrop.lower() != 'auto':
+        try:
+            ycrop = tuple(int(w) for w in args.wcrop.split(':'))
+        except:
+            raise ValueError("Could not parse -wcrop, should be colon-separated integer tuple.")
+
+    try:
+        if "," in args.auto_pad:
+            auto_pad = tuple(int(x) for x in args.auto_pad.split(','))
+        else:
+            auto_pad = int(args.auto_pad)
     except:
-        raise ValueError("Could not parse -wcrop, should be colon-separated integer tuple.")
+        raise ValueError("auto_pad must be integer or comma-separated tuple of integers.")
 
     # Open fits objects
     for filename in file_list:
 
         fitsfile = fits.open(filename)
 
+        #Calculate all automatic crop params if any requesed
+        if 'auto' in [args.xcrop, args.ycrop, args.wcrop]:
+
+            xcrop_auto, ycrop_auto, wcrop_auto = reduction.get_crop_params(fitsfile,
+                zero_only = (args.trim_mode == 'zero'),
+                pad = auto_pad,
+                nsig = args.trim_sclip,
+                plot = args.plot
+            )
+
+            #Assign auto parameters where requested
+            if args.xcrop.lower() == 'auto':
+                xcrop = xcrop_auto
+
+            if args.ycrop.lower() == 'auto':
+                ycrop = ycrop_auto
+
+            if args.wcrop.lower() == 'auto':
+                wcrop = wcrop_auto
+
         # Pass to trimming function
         trimmedFits = reduction.crop(fitsfile,
-            xcrop=(x0,x1),
-            ycrop=(y0,y1),
-            wcrop=(w0,w1),
-            auto=args.auto,
-            plot=args.plot
+            xcrop = xcrop,
+            ycrop = ycrop,
+            wcrop = wcrop
         )
 
         outfile = filename.replace('.fits', args.ext)
