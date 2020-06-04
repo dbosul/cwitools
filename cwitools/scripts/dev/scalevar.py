dd372597d60a39ec797ebdf62f75dd59866b4b79
dbosul <donal.b.osullivan@gmail.com>
2020-06-03 22:00:32 -0700

    Updated and tested iterative variance scaling. Works well.

--- cwitools/scripts/dev/scalevar.py
+++ cwitools/scripts/dev/scalevar.py
@@ -1,5 +1,5 @@
 """Estimate the 3D variance for a data cube"""
-from cwitools import coordinates
+from cwitools import coordinates, utils
 from astropy.io import fits
 from astropy.modeling import models, fitting
 from scipy.ndimage import binary_dilation, generate_binary_structure
@@ -13,7 +13,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 matplotlib.use("TkAgg")
 
-def scale_variance2(data, var, nmax=100, snrmin=2):
+def scale_variance(data, var, nmin=50, snrmin=3, plot=True):
 
     snr_range = (-5, 5)
     snr_nbins = 100
@@ -21,132 +21,77 @@ def scale_variance2(data, var, nmax=100, snrmin=2):
     data = np.nan_to_num(data, nan=0)
     var = np.nan_to_num(var, nan=np.inf)
 
-    snr = data / np.sqrt(var)
-    obj_mask = np.zeros_like(data, dtype=bool)
-
-    vox_msk = snr <= -3#snrmin
-    vox_lab, num_reg = measure.label(vox_msk, return_num=True)
-    for label in range(1, num_reg):
-        reg = vox_lab == label
-        n = np.count_nonzero(reg)
-        if n >= nmax:
-            print(n, nmax, label)
-            vox_lab[reg] = 0
-            obj_mask[reg] = 1
-
-    nnew = np.count_nonzero(obj_mask)
-
-    connect = generate_binary_structure(3, 3)
-    while nnew > 0:
-        obj_mask_exp = binary_dilation(obj_mask, structure=connect)
-        obj_mask_new = (obj_mask_exp) & (snr <= -1)
-        nnew = np.count_nonzero(obj_mask_new) - np.count_nonzero(obj_mask)
-        obj_mask = obj_mask_new.copy()
-
-    obj_mask[snr > 1] = 1
-
-    #Get SNR distribution of non-masked region
-    counts, edges = np.histogram(snr,
-        range=[-4, 1],
-        bins=50
-    )
-    centers = [(edges[i] + edges[i+1]) / 2 for i in range(edges.size - 1)]
-    noisefitter = fitting.LevMarLSQFitter()
-    noisemodel0 = models.Gaussian1D(amplitude=counts.max(), mean=0, stddev=1)
-    noisemodel1 = noisefitter(noisemodel0, centers, counts)
-
-    scale_factor = noisemodel1.stddev.value**2
-    fig, ax = plt.subplots(1, 1, figsize=(10, 10))
-    ax.hist(snr.flatten(),
-        range=snr_range,
-        bins=snr_nbins,
-        facecolor='k'
-    )
-    centers_all = np.linspace(-5, 5, 100)
-    ax.plot(centers_all, noisemodel1(centers_all), 'b-')
-    fig.show()
-    input("")#plt.waitforbuttonpress()
-    plt.close()
-
-    var *= scale_factor
-
-    snr2 = data / np.sqrt(var)
-    fig, ax = plt.subplots(1, 1, figsize=(10, 10))
-    ax.hist(snr2.flatten(),
-        range=snr_range,
-        bins=snr_nbins,
-        facecolor='k'
-    )
-    fig.show()
-    input("")#plt.waitforbuttonpress()
-    plt.close()
-    return scale_factor
-
-def scale_variance(data, var, nmin=100, snr_min=2):
 
 
-    snr_range = (-5, 5)
-    snr_nbins = 100
-
-    #Initialize scale factor as 1
-    scale_factor = 2
+    scale_factor = 1
     scale_factor_change = 1.0
+    std_fit = 99
+    n = 0
+    utils.output("\t%10s %15s %15s %15s\n" % ("iter", "scale_f", "std-dev", "1/std-dev"))
+    while abs(std_fit - 1) >= 0.001:
 
-    data = np.nan_to_num(data, nan=0)
-    var = np.nan_to_num(var, nan=np.inf)
+        n += 1
+        snr = data / np.sqrt(var)
+        #Adjust SNR dist. using latest scale factor
+        snr_scaled = snr * scale_factor
 
-    while scale_factor_change > 0.001:
+        #Segment into regions
+        vox_msk = np.abs(snr_scaled) > snrmin
+        vox_lab, num_reg = measure.label(vox_msk, return_num=True)
 
-        #Get initial SNR cube
+        #Measure sizes of regions above (in absolute terms) snr min
+        reg_props = measure.regionprops_table(vox_lab, properties=['area', 'label'])
+        large_regions = reg_props['area'] > nmin
 
-
-        #Get labelled mask of regions above or below snr_min
-        snr = data / np.sqrt(var*scale_factor)
-        vox_mask = (np.abs(snr) >= snr_min)
-        vox_lab = measure.label(vox_mask)
-
-        #Reject regions below a size of n_min
+        # Create object mask to exclude these regions
         obj_mask = np.zeros_like(data, dtype=bool)
-        labels_unique = np.unique(vox_lab[vox_lab > 0])
-        for label in tqdm(labels_unique):
-            region = vox_lab == label
-            if np.count_nonzero(region) >= nmin:
-                obj_mask[region] = 1
+        for label in reg_props['label'][large_regions]:
+            obj_mask[vox_lab == label] = 1
 
-        #Get SNR distribution of non-masked region
-        counts, edges = np.histogram(snr[~obj_mask],
-            range=snr_range,
-            bins=snr_nbins
+
+        #Get SNR distribution of non-masked regions
+        counts, edges = np.histogram(
+            snr_scaled[~obj_mask],
+            range=[-3, 3],
+            bins=50
         )
-        centers = [(edges[i] + edges[i+1]) / 2 for i in range(edges.size - 1)]
 
+        #Fit Gaussian model
+        centers = np.array([(edges[i] + edges[i+1]) / 2 for i in range(edges.size - 1)])
         noisefitter = fitting.LevMarLSQFitter()
         noisemodel0 = models.Gaussian1D(amplitude=counts.max(), mean=0, stddev=1)
         noisemodel1 = noisefitter(noisemodel0, centers, counts)
+        std_fit1 = noisemodel1.stddev.value
+        fit_cens = np.abs(centers) > 0.5 * std_fit1
+        noisemodel2 = noisefitter(noisemodel0, centers[fit_cens], counts[fit_cens])
+        std_fit = noisemodel2.stddev.value
+
+        if plot:
+            counts_all, edges_all = np.histogram(
+                snr_scaled[~obj_mask],
+                range=[-3, 3],
+                bins=50
+            )
+            fig, ax  = plt.subplots(1, 1, figsize=(12,12))
+            ax.plot( centers, counts_all, 'k.--', alpha=0.5)
+            ax.plot( centers, counts, 'k.--')
+            ax.plot( centers[fit_cens], counts[fit_cens], 'kx')
+            ax.plot( centers, noisemodel2(centers), 'r-')
+            fig.show()
+            input("")#plt.waitforbuttonpress()
+            plt.close()
+
+        new_scale_factor = 1 / std_fit
+        utils.output("\t%10i %15.5f %15.5f %15.5f\n" % (n, scale_factor, std_fit, 1 / std_fit))
+
+        scale_factor *= new_scale_factor
 
 
-        scale_factor_new =  scale_factor * noisemodel1.stddev.value**2
-        scale_factor_change = abs(scale_factor_new - scale_factor)
-        scale_factor = scale_factor_new
 
-        print(scale_factor, scale_factor_change)
-        fig, ax = plt.subplots(1, 1, figsize=(10, 10))
-        ax.hist(snr.flatten(),
-            range=snr_range,
-            bins=snr_nbins,
-            facecolor='k'
-        )
-        ax.hist(snr[~obj_mask].flatten(),
-            range=snr_range,
-            bins=snr_nbins,
-            facecolor='r'
-        )
-        ax.plot(centers, noisemodel1(centers), 'b-')
-        fig.show()
-        plt.waitforbuttonpress()
-        plt.close()
 
-    return scale_factor
+
+    return scale_factor**2
+
 
 def main():
     #Take any additional input params, if provided
@@ -174,6 +119,10 @@ def main():
                         metavar='Wav Mask',
                         help='Wavelength range(s) to mask when fitting',
                         default=None
+    )
+    parser.add_argument('-plot',
+                        action='store_true',
+                        help="Display diagnostic plots."
     )
     parser.add_argument('-out',
                         type=str,
@@ -213,22 +162,25 @@ def main():
     data_scale = data[zmask]
     var_scale = var[zmask]
 
-    scale_factor = scale_variance2(data_scale, var_scale,
-        nmax=args.n_min,
-        snrmin=args.snr_min
+    scale_factor = scale_variance(data_scale, var_scale,
+        nmin=args.n_min,
+        snrmin=args.snr_min,
+        plot = args.plot
     )
     #scale_factor = scale_variance(data_scale, var_scale,
     #    nmin=args.n_min,
     #    snr_min=args.snr_min
     #)
-    print(scale_factor)
+
     if args.out == None:
         outfilename = args.var.replace('.fits', '.scaled.fits')
     else:
         outfilename = args.out
 
+    utils.output("Std-dev of Noise SNR Distribution = %.3f\n" % np.sqrt(scale_factor))
+    utils.output("Variance Scaled by %.3f to assert Standard Normal Distribution\n" % scale_factor)
     var_fits[0].data *= scale_factor
     var_fits.writeto(outfilename, overwrite=True)
-    print("Saved %s" % outfilename)
+    utils.output("Saved %s\n" % outfilename)
 
 if __name__=="__main__": main()
