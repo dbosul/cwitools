6f4e3bc423b0cb8080487c3e6ed5dd3d12cf06e2
yuguangchen1 <yuguang.chen.1@gmail.com>
2020-06-12 20:52:17 -0700

    remove notebook files

--- cwitools/scripts/.ipynb_checkpoints/shellaccess-checkpoint.py
+++
@@ -1,8 +0,0 @@
-import cwitools.scripts as scripts
-
-def apply_mask(mask=None, data=None, fill=0, ext=".M.fits", log=None,
-    silent=False, label=0):
-    """Wrapper for applying mask to data - Docstring TBC"""
-    scripts.apply_mask.main(mask, data, fill, ext, log, silent, label)
-
-cwi_measurewcs = scripts.measurewcs.main
