2e3e69f86de01e5b97b34413b6fdf2bd5ab61d5c
dbosul <donal.b.osullivan@gmail.com>
2020-09-09 12:27:09 -0700

    Updated to new usage syntax. Tidied up main code too.

--- cwitools/scripts/measurewcs.py
+++ cwitools/scripts/measurewcs.py
@@ -1,4 +1,4 @@
-"""Create a WCS correction table by measuring the input data."""
+"""Measure WCS: Create a WCS correction table by measuring the input data."""
 from astropy.io import fits
 from astropy.wcs import WCS
 from cwitools import coordinates, reduction, utils
@@ -16,223 +16,269 @@ import pdb
 
 def parser_init():
 
-    parser = argparse.ArgumentParser(description='Measure WCS parameters and save to WCS correction file.')
-    parser.add_argument('clist',
-                        metavar="cube_list",
-                        type=str,
-                        help='CWITools cube list.'
-    )
-    parser.add_argument('-ctype',
-                        type=str,
-                        metavar="<cube_type>",
-                        help='Type of input cube to work with.',
-                        default="icubes.fits"
-    )
-    parser.add_argument('-xymode',
-                        help='Which method to use for correcting X/Y axes',
-                        default="src_fit",
-                        choices=["src_fit", "xcor", "none"]
-    )
-    parser.add_argument('-ra',
-                        metavar="<dd.ddd>",
-                        type=float,
-                        help="Right-ascension of source to fit.",
-                        default=None
-    )
-    parser.add_argument('-dec',
-                        metavar="<dd.ddd>",
-                        type=float,
-                        help="Declination of source to fit.",
-                        default=None
-    )
-    parser.add_argument('-box',
-                        metavar="<box_size>",
-                        type=float,
-                        help="Box size (arcsec) for fitting source.",
-                        default=None #10
-    )
-    parser.add_argument('-crpix1s',
-                        metavar="<list of floats>",
-                        nargs='+',
-                        help="List of CRPIX1. Set to -1s to use the header of the corresponding files.",
-                        default=None
-                       )
-    parser.add_argument('-crpix2s',
-                        metavar="<list of floats>",
-                        nargs='+',
-                        help="List of CRPIX2. Set to -1s to use the header of the corresponding files.",
-                        default=None
-                       )
-    parser.add_argument('-background_sub',
-                        metavar="",
-                        help="Subtract background before xcor.",
-                        default=False
-                       )
-    parser.add_argument('-zmode',
-                        help='Which method to use for correcting z-azis',
-                        default="none",
-                        choices=["none", "xcor"]
-    )
-    parser.add_argument('-plot',
-                        help="Display fits with Matplotlib.",
-                        action='store_true'
-    )
-    parser.add_argument('-out',
-                        metavar="",
-                        help="Output table name.",
-                        default=None
-    )
-    parser.add_argument('-log',
-                        metavar="<log_file>",
-                        type=str,
-                        help="Log file to save output in.",
-                        default=None
-    )
-    parser.add_argument('-silent',
-                        help="Set flag to suppress standard terminal output.",
-                        action='store_true'
-    )
+    parser = argparse.ArgumentParser(
+        description='Measure WCS parameters and save to WCS correction file.'
+        )
+    parser.add_argument(
+        'clist',
+        metavar="cube_list",
+        type=str,
+        help='CWITools cube list.'
+        )
+    parser.add_argument(
+        '-ctype',
+        type=str,
+        metavar="<cube_type>",
+        help='Type of input cube to work with.',
+        default="icubes.fits"
+        )
+    parser.add_argument(
+        '-xymode',
+        help='Which method to use for correcting X/Y axes',
+        default="src_fit",
+        choices=["src_fit", "xcor", "none"]
+        )
+    parser.add_argument(
+        '-ra',
+        metavar="<dd.ddd>",
+        type=float,
+        help="Right-ascension of source to fit.",
+        default=None
+        )
+    parser.add_argument(
+        '-dec',
+        metavar="<dd.ddd>",
+        type=float,
+        help="Declination of source to fit.",
+        default=None
+        )
+    parser.add_argument(
+        '-box',
+        metavar="<box_size>",
+        type=float,
+        help="Box size (arcsec) for fitting source or cross-correlating.",
+        default=None
+        )
+    parser.add_argument(
+        '-crpix1s',
+        metavar="<list of floats>",
+        nargs='+',
+        help="List of CRPIX1. Use 'H' to use existing header value.",
+        default=None
+        )
+    parser.add_argument(
+        '-crpix2s',
+        metavar="<list of floats>",
+        nargs='+',
+        help="List of CRPIX2. Use 'H' to use existing header value.",
+        default=None
+        )
+    parser.add_argument(
+        '-background_sub',
+        metavar="",
+        help="Subtract background before xcor.",
+        default=False
+        )
+    parser.add_argument(
+        '-zmode',
+        help='Which method to use for correcting z-azis',
+        default=None,
+        choices=['none', "xcor"]
+        )
+    parser.add_argument(
+        '-plot',
+        help="Display fits with Matplotlib.",
+        action='store_true'
+        )
+    parser.add_argument(
+        '-out',
+        metavar="",
+        help="Output table name.",
+        default=None
+        )
+    parser.add_argument(
+        '-log',
+        metavar="<log_file>",
+        type=str,
+        help="Log file to save output in.",
+        default=None
+        )
+    parser.add_argument(
+        '-silent',
+        help="Set flag to suppress standard terminal output.",
+        action='store_true'
+        )
 
     return parser
 
+def main(clist, ctype="icubes.fits", xymode="src_fit", ra=None, dec=None, box=10.0,
+         crpix1s=None, crpix2s=None, background_sub=False, zmode='none',
+         plot=False, out=None, log=None, silent=False, arg_parser=None):
+    """Automatically create a WCS correction table for a list of input cubes.
 
-def core(args, parser):
-    
-    #Set global parameters
-    cwitools.silent_mode = args.silent
-    cwitools.log_file = args.log
-
-    #Give output summarizing mode
-    cmd = utils.get_cmd(sys.argv)
-    titlestring = """\n{0}\n{1}\n\tCWI_MEASUREWCS:""".format(datetime.now(), cmd)
-    infostring = utils.get_arg_string(args)
-    utils.output(titlestring + infostring)        
-
-    #Parse cube list
-    clist = utils.parse_cubelist(args.clist)
-    
-    #Load the default alignment RA and DEC
-    if args.xymode == 'src_fit':
+    Args:
+        clist (str): Path to the CWITools .list file
+        ctype (str): File extension for type of cube to use as input.
+            Default value: 'icubes.fits' (.fits extension should be included)
+        xymode (str): Method to use for XY alignment:
+            'src_fit': Fit 1D profiles to a known point source (interactive)
+            'xcor': Perform 2D (XY) cross-correlation of input images.
+            'none': Do not align the spatial axes.
+        ra (float): Decimal RA coordinate of source, if using 'src_fit'
+        dec (float): Decimal DEC coordinate of source, if using 'src_fit'
+        box (float): Size of box (in arcsec) to use for finding/fitting source,
+            if using 'src_fit'
+        crpix1s (list): List of CRPIX1 values, if
+    """
+    #If called using keyword args (i.e. command-line usage)
+    if arg_parser is not None:
+        args = parser.parse_args()
+        clist = args.clist
+        ctype = args.ctype
+        xymode = args.xymode
         ra = args.ra
         dec = args.dec
-        if ra == None and dec == None:
-            raise ValueError("-ra and -dec must be set if using src_fit.")
-        if args.box == None:
-            utils.output('\t-ra and -dec not set for src_fit, setting to 10 arcsec.\n')
-            args.box = 10. 
-    elif args.xymode == 'xcor':
-        crpix1s = np.zeros(len(clist['ID_LIST'])) - 1
-        crpix2s = np.zeros(len(clist['ID_LIST'])) - 1
-        if (args.crpix1s is None) != (args.crpix2s is None):
-            raise ValueError("-crpix1s and -crpix2s must be set simultaneously.")
-        if args.crpix1s is not None:
-            for i, crpix1 in enumerate(args.crpix1s):
-                crpix1s[i] = float(crpix1)
-        if args.crpix2s is not None:
-            for i, crpix2 in enumerate(args.crpix2s):
-                crpix2s[i] = float(crpix2)
+        box = args.box
+        crpix1s = args.crpix1s
+        crpix2s = args.crpix2s
+        background_sub = args.background_sub
+        zmode = args.zmode
+        plot = args.plot
+        out = args.out
+        log = args.log
+        silent = args.silent
+
+        #Give output summarizing mode
+        cmd = utils.get_cmd(sys.argv)
+        titlestring = """\n{0}\n{1}\n\tCWI_MEASUREWCS:""".format(datetime.now(), cmd)
+        infostring = utils.get_arg_string(args)
+        utils.output(titlestring + infostring)
+
+    #Set global parameters
+    cwitools.silent_mode = silent
+    cwitools.log_file = log
+
+    #Parse cube list
+    cdict = utils.parse_cubelist(clist)
 
     #Load input files
     in_files = utils.find_files(
-        clist["ID_LIST"],
-        clist["INPUT_DIRECTORY"],
-        args.ctype,
-        depth=clist["SEARCH_DEPTH"]
+        cdict["ID_LIST"],
+        cdict["INPUT_DIRECTORY"],
+        ctype,
+        depth=cdict["SEARCH_DEPTH"]
     )
+    #Load scube (or ocube) files
+    int_fits = [fits.open(x) for x in in_files]
+    sky_fits = [fits.open(x.replace("icube", "scube")) for x in in_files]
 
     #Prepare table output
-    outstr = "INPUT_DIRECTORY=%s\n" % clist["INPUT_DIRECTORY"]
-    outstr += "SEARCH_DEPTH=%i\n" % clist["SEARCH_DEPTH"]
+    outstr = "INPUT_DIRECTORY=%s\n" % cdict["INPUT_DIRECTORY"]
+    outstr += "SEARCH_DEPTH=%i\n" % cdict["SEARCH_DEPTH"]
     outstr += "#%19s %15s %15s %10s %10s %10s %10s\n" % (
-    "ID", "CRVAL1", "CRVAL2", "CRVAL3", "CRPIX1", "CRPIX2", "CRPIX3")
+        "ID", "CRVAL1", "CRVAL2", "CRVAL3", "CRPIX1", "CRPIX2", "CRPIX3")
 
-    crval3s = [-1 for file in in_files]
-    crpix3s = [-1 for file in in_files]
-
-    #If wavelength alignment has been requested
-    if args.zmode == "xcor":
+    #WAVELENGTH ALIGNMENT - XCOR
+    if zmode == "xcor":
         utils.output("\tAligning z-axes...\n")
-        sky_fits = [fits.open(x.replace('icube','scube')) for x in in_files]
-        crpix3_vals_new = reduction.xcor_crpix3(sky_fits)
+        crval3s = [i_f[0].header["CRVAL3"] for i_f in int_fits]
+        crpix3s = reduction.xcor_crpix3(sky_fits)
 
-        for i, crpix3 in enumerate(crpix3_vals_new):
+    elif zmode == "none":
+        warnings.warn("No wavelength WCS correction applied.")
+        crval3s = [i_f[0].header["CRVAL3"] for i_f in int_fits]
+        crpix3s = [i_f[0].header["CRPIX3"] for i_f in int_fits]
+    else:
+        raise ValueError("zmode can only be 'none' or 'xcor'")
 
-            #If no change in CRPIX3 value - set to -1 for later
-            if crpix3 == sky_fits[i][0].header["CRPIX3"]:
-                crval3s[i] = -1
-                crpix3s[i] = -1
-
-            #If changed, set to new value for later
-            else:
-                crpix3s[i] = crpix3_vals_new[i]
-
-        if np.all(np.array(crpix3s) == -1):
-            utils.output("\t\tInput z-axes already well aligned.\n\n")
-
-
-    if args.xymode == "src_fit":
+    #Basic checks and user-message for each xy-mode
+    if xymode == "src_fit":
+        if ra is None and dec is None:
+            raise ValueError("'ra' and 'dec' must be set if using src_fit.")
+        if box is None:
+            box = 10.0
         utils.output("\tFitting source positions...\n")
-        for i, in_file in enumerate(in_files):
 
-            in_fits = fits.open(in_file)
+    elif xymode == "xcor":
+        if (crpix1s is None) != (crpix2s is None):
+            raise ValueError("'crpix1s' and 'crpix2s' must be set together")
+        utils.output("\tCross-correlating in 2D...\n")
 
-            crpix1, crpix2 = reduction.fit_crpix12(in_fits, ra, dec,
-                plot=args.plot,
-                box_size=args.box
+    #SPATIAL ALIGNMENT
+    for i, i_f in enumerate(int_fits):
+
+        hdr = i_f[0].header
+
+        if xymode == "src_fit":
+            crval1, crval2 = ra, dec
+            crpix1, crpix2 = reduction.fit_crpix12(
+                i_f, crval1, crval2,
+                plot=plot,
+                box_size=box
             )
-            
-            istring = "\t\t{0}: {1:.2f}, {2:.1f}\n".format(clist["ID_LIST"][i], crpix1, crpix2)
+
+            istring = "\t\t{0}: {1:.2f}, {2:.1f}\n".format(cdict["ID_LIST"][i], crpix1, crpix2)
             utils.output(istring)
 
-            outstr += ">%19s %15.7f %15.7f %10.3f %10.1f %10.1f %10.1f\n" % (
-            clist["ID_LIST"][i], ra, dec, crval3s[i], crpix1, crpix2, crpix3s[i])
-            
-    elif args.xymode == "xcor":
-        utils.output("\tCross-correlating in 2D...\n")
-        for i, in_file in enumerate(in_files):
+        #SPATIAL ALIGNMENT  - XCOR
+        elif xymode == "xcor":
 
-            in_fits = fits.open(in_file)
-            if (crpix1s[i] != -1) and (crpix2s[i] != -1):
-                crpix = [crpix1s[i], crpix2s[i]]
+            #If CRPIX1s given, and current value is not 'Header' indicator
+            if crpix1s is not None and crpix1s[i] != 'H' and crpix2s[i] != 'H':
+                crpix = [float(crpix1s[i]), float(crpix2s[i])]
             else:
                 crpix = None
-                
+
             # Use i=0  as the reference image
             if i == 0:
-                hdr = in_fits[0].header
-                
-                if args.ra is not None:
+
+                if ra is not None:
                     wcs0 = WCS(hdr)
-                    tmp = wcs0.all_world2pix(args.ra, args.dec, 1, 1)
-                    
-                    crpix1, crpix2, crval1, crval2 = float(tmp[0]), float(tmp[1]), args.ra, args.dec
+                    tmp = wcs0.all_world2pix(ra, dec, 1, 1)
+                    crpix1, crpix2 = float(tmp[0]), float(tmp[1])
+                    crval1, crval2 = ra, dec
+
                 else:
-                    crpix1, crpix2, crval1, crval2 = hdr['CRPIX1'], hdr['CRPIX2'], hdr['CRVAL1'], hdr['CRVAL2']
-                
-                ref_fits = in_fits
+                    crpix1, crpix2 = hdr['CRPIX1'], hdr['CRPIX2']
+                    crval1, crval2 = hdr['CRVAL1'], hdr['CRVAL2']
+
+                ref_fits = i_f
+
             else:
-                crpix1, crpix2, crval1, crval2 = reduction.xcor_cr12(in_fits,
+                crpix1, crpix2, crval1, crval2 = reduction.xcor_cr12(
+                    i_f,
                     ref_fits,
-                    ra=args.ra, 
-                    dec=args.dec,
+                    ra=ra,
+                    dec=dec,
                     crpix=crpix,
-                    background_subtraction = args.background_sub,
-                    plot=int(args.plot)*2,
-                    box_size=args.box
+                    background_subtraction=background_sub,
+                    plot=int(plot)*2,
+                    box_size=box
                 )
-            
-            istring = "\t\t{0}: {1:.2f}, {2:.1f}, {3:.4f}, {4:.4f}\n".format(clist["ID_LIST"][i], crpix1, crpix2, crval1, crval2)
+
+            istring = "\t\t{0}: {1:.2f}, {2:.1f}, {3:.4f}, {4:.4f}\n".format(
+                cdict["ID_LIST"][i], crpix1, crpix2, crval1, crval2)
             utils.output(istring)
 
-            outstr += ">%19s %15.7f %15.7f %10.3f %10.1f %10.1f %10.1f\n" % (
-            clist["ID_LIST"][i], crval1, crval2, crval3s[i], crpix1, crpix2, crpix3s[i])
-        
+
+        elif xymode == 'none':
+
+            crpix1 = hdr["CRPIX1"]
+            crpix2 = hdr["CRPIX2"]
+            crval1 = hdr["CRVAL1"]
+            crval2 = hdr["CRVAL2"]
+
+        else:
+            raise ValueError("xymode can only be 'none', 'src_fit', or 'xcor'")
 
 
-    if args.out == None:
-        outfilename = args.clist.replace(".list", ".wcs")
+        outstr += ">%19s %15.7f %15.7f %10.3f %10.1f %10.1f %10.1f\n" % (
+            cdict["ID_LIST"][i], crval1, crval2, crval3s[i], crpix1, crpix2, crpix3s[i])
+
+    if out is None:
+        outfilename = clist.replace(".list", ".wcs")
     else:
-        outfilename = args.out
+        outfilename = out
 
     #Create the correction file
     outfile = open(outfilename, 'w')
@@ -241,62 +287,8 @@ def core(args, parser):
 
     utils.output("\n\tSaved corrections table to %s\n" % outfilename)
 
-if __name__=="__main__":
+
+#If called from the command line
+if __name__ == "__main__":
     parser = parser_init()
-    args = parser.parse_args()
-    core(args, parser)
-
-
-def main(clist, ctype=None, xymode=None, ra=None, dec=None, box=None,
-         crpix1s=None, crpix2s=None, background_sub=None, zmode=None, 
-         plot=None, out=None, log=None, silent=None):
-        
-    parser = parser_init()
-    
-    # construct args
-    str_list=[clist]
-    if ctype is not None:
-        str_list.append('-ctype')
-        str_list.append(str(ctype))
-    if xymode is not None:
-        str_list.append('-xymode')
-        str_list.append(str(xymode))
-    if ra is not None:
-        str_list.append('-ra')
-        str_list.append(str(ra))
-    if dec is not None:
-        str_list.append('-dec')
-        str_list.append(str(dec))
-    if box is not None:
-        str_list.append('-box')
-        str_list.append(str(box))
-    if crpix1s is not None:
-        str_list.append('-crpix1s')
-        for i in crpix1s:
-            str_list.append(str(i))
-    if crpix2s is not None:
-        str_list.append('-crpix2s')
-        for i in crpix2s:
-            str_list.append(str(i))
-    if background_sub is not None:
-        str_list.append('-background_sub')
-        str_list.append(str(background_sub))
-    if zmode is not None:
-        str_list.append('-zmode')
-        str_list.append(str(zmode))
-    if plot is not None:
-        if plot:
-            str_list.append('-plot')
-    if out is not None:
-        str_list.append('-out')
-        str_list.append(str(out))
-    if log is not None:
-        str_list.append('-log')
-        str_list.append(str(log))
-    if silent is not None:
-        if silent:
-            str_list.append('-silent')
-    args = parser.parse_args(str_list)
-        
-    core(args, parser)
-
+    main("", arg_parser=parser)
