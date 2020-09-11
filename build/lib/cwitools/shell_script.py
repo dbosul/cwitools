f23c4c1decee5100569caf75a3cbdd174c2a95f8
yuguangchen1 <yuguang.chen.1@gmail.com>
2020-09-11 15:23:06 -0700

    Removing old build/ which caused issues when installing.

--- build/lib/cwitools/shell_script.py
+++
@@ -1,9 +0,0 @@
-import cwitools.scripts as scripts
-
-def apply_mask(mask=None, data=None, fill=0, ext=".M.fits", log=None,
-    silent=False, label=0):
-    """Wrapper for applying mask to data - Docstring TBC"""
-    scripts.apply_mask.main(mask, data, fill, ext, log, silent, label)
-    
-import cwitools.scripts.measurewcs
-cwi_measurewcs = cwitools.scripts.measurewcs.main
