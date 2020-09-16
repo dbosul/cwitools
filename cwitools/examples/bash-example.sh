78d92c7718b80843695aa236a6dfe9026e03e96b
dbosul <donal.b.osullivan@gmail.com>
2020-09-16 14:06:28 -0700

    Working on examples

---
+++ cwitools/examples/bash-example.sh
@@ -0,0 +1,8 @@
+#
+# CWITOOLS - BASH TERMINAL EXAMPLES
+#
+# This script provides example of how to use CWITools from the command-line in a linux environment,
+# and should extend simply to MacOS environments as well.
+#
+# The script is written in stages, as an example of how you can build a pipeline from CWITools,
+# but each of the stages can 
