236f19b0433a5d875c4f49111679aab7a4deb861
dbosul <donal.b.osullivan@gmail.com>
2020-09-17 16:44:02 -0700

    Updating setup.py

--- setup.py
+++ setup.py
@@ -5,47 +5,51 @@ with open("README.md","r") as fh:
 
 setuptools.setup(
         name='cwitools',
-        version='0.6',
+        version='0.8',
         author="Donal O'Sullivan",
         author_email="dosulliv@caltech.edu",
         description="Analysis and Reduction Tools for PCWI/KCWI Data",
         long_description=long_description,
         url="https://github.com/dbosul/cwitools",
-        download_url="https://github.com/dbosul/cwitools/archive/v0.5.tar.gz",
+        download_url="https://github.com/dbosul/cwitools/archive/v0.8.tar.gz",
         packages=setuptools.find_packages(),
 	    include_package_data=True,
         package_data={'': ['data/sky/*.txt', 'data/gal_lines/*.csv']},
-        entry_points = {
-             'console_scripts': [
-                 'cwi_applymask = cwitools.scripts.applymask:main',
-                 'cwi_applywcs = cwitools.scripts.applywcs:main',
-                 'cwi_bgsub = cwitools.scripts.bgsub:main',
-                 'cwi_coadd = cwitools.scripts.coadd:main',
-                 'cwi_crop = cwitools.scripts.crop:main',
-                 'cwi_getmask = cwitools.scripts.getmask:main',
-                 'cwi_getnb = cwitools.scripts.getnb:main',
-                 'cwi_getvar = cwitools.scripts.getvar:main',
-                 'cwi_initparams = cwitools.scripts.initparams:main',
-                 'cwi_measurewcs = cwitools.scripts.measurewcs:main',
-                 'cwi_moments = cwitools.scripts.moments:main',
-                 'cwi_psfsub = cwitools.scripts.psfsub:main',
-                 'cwi_rebin = cwitools.scripts.rebin:main',
-                 'cwi_slicecorr = cwitools.scripts.slicecorr:main',
-                 'cwi_segment = cwitools.scripts.segment:main'
-             ]
-        },
-        install_requires = [
-        'astropy',
-        'argparse',
-        'matplotlib',
-        'numpy',
-        'photutils',
-        'scipy',
-        'shapely',
-        'tqdm',
-        'PyAstronomy',
-        'pyregion',
-        'reproject',
-        'scikit-image'
-        ]
+        entry_points={
+            'console_scripts': [
+                'cwi_apply_mask = cwitools.scripts.apply_mask:apply_mask'
+                'cwi_apply_wcs = cwitools.scripts.apply_wcs:apply_wcs'
+                'cwi_bg_sub = cwitools.scripts.bg_sub:bg_sub'
+                'cwi_coadd = cwitools.scripts.coadd:coadd'
+                'cwi_crop = cwitools.scripts.crop:crop'
+                'cwi_fit_covar = cwitools.scripts.fit_covar:fit_covar'
+                'cwi_get_mask = cwitools.scripts.get_mask:get_mask'
+                'cwi_get_nb = cwitools.scripts.get_nb:get_nb'
+                'cwi_get_var = cwitools.scripts.get_var:get_var'
+                'cwi_get_wl = cwitools.scripts.get_wl:get_wl'
+                'cwi_mask_z = cwitools.scripts.mask_z:mask_z'
+                'cwi_measure_wcs = cwitools.scripts.measure_wcs:measure_wcs'
+                'cwi_obj_sb = cwitools.scripts.obj_sb:obj_sb'
+                'cwi_obj_zmoments = cwitools.scripts.obj_zmoments:obj_zmoments',
+                'cwi_psf_sub = cwitools.scripts.psf_sub:psf_sub'
+                'cwi_rebin = cwitools.scripts.rebin:rebin'
+                'cwi_scale_var = cwitools.scripts.scale_var:scale_var'
+                'cwi_segment = cwitools.scripts.segment:segment'
+                'cwi_slice_corr = cwitools.scripts.slice_corr:slice_corr'
+                ]
+            },
+        install_requires=[
+            'astropy',
+            'argparse',
+            'matplotlib',
+            'numpy',
+            'photutils',
+            'scipy',
+            'shapely',
+            'tqdm',
+            'PyAstronomy',
+            'pyregion',
+            'reproject',
+            'scikit-image'
+            ]
 )
