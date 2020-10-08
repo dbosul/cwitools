1e4e36bd6ab9d530e497ac36531760571923c05f
dbosul <donal.b.osullivan@gmail.com>
2020-10-08 12:17:25 -0700

    Added import aliases for core functions (e.g. cwi_coadd for coadd.coadd)

---
+++ cwitools/scripts/__init__.py
@@ -0,0 +1,24 @@
+"""Core executable scripts module"""
+from .apply_mask import apply_mask as cwi_apply_mask
+from .apply_wcs import apply_wcs as cwi_apply_wcs
+from .bg_sub import bg_sub as cwi_bg_sub
+from .coadd import coadd as cwi_coadd
+from .crop import crop as cwi_crop
+from .fit_covar import fit_covar as cwi_fit_covar
+from .get_mask import get_mask as cwi_get_mask
+from .get_nb import get_nb as cwi_get_nb
+from .get_rprof import get_rprof as cwi_get_rprof
+from .get_var import get_var as cwi_get_var
+from .get_wl import get_wl as cwi_get_wl
+from .mask_z import mask_z as cwi_mask_z
+from .measure_wcs import measure_wcs as cwi_measure_wcs
+from .obj_lum import obj_lum as cwi_obj_lum
+from .obj_morpho import obj_morpho as cwi_obj_morpho
+from .obj_sb import obj_sb as cwi_obj_sb
+from .obj_spec import obj_spec as cwi_obj_spec
+from .obj_zmoments import obj_zmoments as cwi_obj_zmoments
+from .psf_sub import psf_sub as cwi_psf_sub
+from .rebin import rebin as cwi_rebin
+from .scale_var import scale_var as cwi_scale_var
+from .segment import segment as cwi_segment
+from .slice_corr import slice_corr as cwi_slice_corr
