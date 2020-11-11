27fe6d9ad87773ad67db6f0b3511b3727d3b3100
dbosul <donal.b.osullivan@gmail.com>
2020-11-11 13:26:22 -0800

    Removed requirement for pyregion, which is causing many issues.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='cwitools',
-    version='0.8.4',
+    version='0.8.5',
     author="Donal O'Sullivan",
     author_email="dosulliv@caltech.edu",
     description="Analysis and Reduction Tools for PCWI/KCWI Data",
@@ -54,7 +54,6 @@ setuptools.setup(
         'shapely',
         'tqdm',
         'PyAstronomy',
-        'pyregion',
         'reproject',
         'scikit-image'
         ]
