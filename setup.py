0064396dafff48e8ed6fe2d87cc954c2b8383277
dbosul <donal.b.osullivan@gmail.com>
2020-11-12 10:45:59 -0800

    Also 0.8.6 build

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='cwitools',
-    version='0.8.5',
+    version='0.8.6',
     author="Donal O'Sullivan",
     author_email="dosulliv@caltech.edu",
     description="Analysis and Reduction Tools for PCWI/KCWI Data",
